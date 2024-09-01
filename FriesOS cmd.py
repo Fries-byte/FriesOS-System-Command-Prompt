2590afa42a8d1a03d436f3a0527b8ec97931216d
Fries-byte <69freshfrenchfries@gmail.com>
2024-09-01 11:05:44 +0200

    Add files via upload

---
+++ FriesOS cmd.py
@@ -0,0 +1,27 @@
+#----ShortCut(s)-----#
+
+txt = ".txt"
+
+#----ShortCut(s)---#
+
+
+#-----System-------#
+print("FriesOS Operating System [Version 1.0]")
+print("(c)Fries-Byte in GitHub 2024")
+
+startup = input("C:/Users/FreshFrenchFries>")
+
+if startup == "help":
+        input("sudo; run a file. cfile; will create a file. restart the program if want to continue")
+           
+if startup == "cfile":
+    fn = input("File Name NOTE: WILL ALWAYS BE A .txt FILE; ")
+    print(fn+ ".txt was successfully created")
+    fin = input("In; ")
+    print(fin+". Has been added into "+fn+txt)
+    
+#-----System-------#
+
+raise ValueError()
+        print("'text' is is not recognized as an internal or external command,
+operable program or batch file.")
