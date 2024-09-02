80a7ed7fa40e15064d076119db329fcec1026392
Fries-byte <69freshfrenchfries@gmail.com>
2024-09-02 08:00:15 +0200

    Update FriesOS System Command Prompt.py

--- FriesOS System Command Prompt.py
+++ FriesOS System Command Prompt.py
@@ -1,14 +1,18 @@
-# Please dont edit line 2
-# Fries-Byte made this, open-source
-
+# Code is owned by Fries
+# DO NOT EDIT IT UNLESS YOU KNOW WHAT YOUR DOING!!!
 inuser = input("Name the Operating System:~$ ")
 print("hosting Server...")
-print("server loaded!")
+print("server hosted!")
+print("connecting...")
 print("                                                                                                                 ")
 print("FriesOS Operating System [Version 1.0]")
 print("(c)Fries-Byte in GitHub 2024")
 while 1 == 1:
     serverip = input(inuser+ "@serverhost.test:~$ ")
+    
+    if serverip == "echo":
+        printer = input("Print: ")
+        print(printer)
     
     if serverip == "cfile":
         cfile = input("FileName:~$ ")
@@ -25,8 +29,10 @@ while 1 == 1:
     
     if serverip == "info -s":
         print("server name: "+inuser)
-        print("server ip: "+serverip)
-        print("server state: perfect")
+        print(inuser+"@serverhost.test  . server ip")
+        print("server creation date: -unknown-")
+        print("server's ip: 129.400.14.03")
+        print("                                                        ")
     
     if serverip == "textfile -load":
         print(infile)
